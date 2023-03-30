df3874ddc34717520eb9b3b0a145f83a5b54a558
baltazargch <baltazargch@gmail.com>
2023-03-30 13:51:28 -0300

    pdf generation two species

---
+++ R/generate_pdfs.R
@@ -0,0 +1,40 @@
+library(knitr)
+library(sessioninfo)
+library(rmarkdown)
+library(purrr)
+
+library(furrr)
+future::plan(multisession)
+
+library(tictoc)
+
+species <- c(
+  'Blastocerus dichotomus', 
+  'Necromys lilloi'
+)
+
+dir.create('pdfs', recursive = T, showWarnings = F)
+
+my_render <- function(x){
+  if(file.exists(paste0('pdfs/', x, '.pdf'))){
+    NULL
+  } else {
+    xfun::Rscript_call(
+      render, 
+      list(
+        input = "species_pdf.Rmd", 
+        output_format = 'pdf_document', 
+        params = list(species = x), 
+        output_file = paste0('pdfs/', x, '.pdf'),
+        envir = new.env(), 
+        clean = TRUE
+      )
+    )
+  }
+}
+
+tic()
+
+future_map(species, ~ try({my_render(.x)}))
+
+toc()
