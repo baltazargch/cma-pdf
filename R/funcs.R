8b7c725ab5a237e023a41b40ffda4b008b694566
baltazargch <baltazargch@gmail.com>
2023-03-30 13:23:00 -0300

    make subpob cons function

--- R/funcs.R
+++ R/funcs.R
@@ -39,5 +39,31 @@ CMA_parse_authors <- function(data, authors_csv ='data/lista_de_autores.csv'){
   return(list(autores = authors_data))
 }
 
+#### Parse conservation ----
 
-
+##### Parse subpopulation ----
+CMA_subpop_cons <- function(data){
+  require(tidyverse)
+  require(kableExtra)
+  spListPop <- data$group_sp_eval_subpob %>% str_split('\n') %>% unlist() %>% str_trim()
+  
+  
+  spListPop <- spListPop[ spListPop != ""]
+  
+  subPob <- spListPop[which(str_detect(spListPop, 'Subpoblación')) + 1]
+  catPob <- spListPop[which(str_detect(spListPop, 'Subpoblación')) + 3]
+  criPob <- spListPop[which(str_detect(spListPop, 'Subpoblación')) + 5]
+  
+  tbsPob <- vector()
+  for(i in seq_along(subPob)){
+    tbsPob[i] <- tribble(
+      ~'Subpoblación', ~'Categoría', ~'Criterios y subcriterios',
+      subPob[i], catPob[i], criPob[i]
+    ) %>% 
+      kbl(booktabs = T, format = 'latex') %>% 
+      row_spec(0,bold=TRUE, extra_latex_after = "\\arrayrulecolor{white}") %>% 
+      kable_styling(latex_options = c('striped', "HOLD_position"),
+                    position = "center", full_width = T)
+  }
+  return(tbsPob)
+}
