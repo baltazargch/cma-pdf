021d0bb1eadee154a498c324186057fa8f71b825
baltazargch <baltazargch@gmail.com>
2023-03-30 16:12:24 -0300

    update funcs

--- R/funcs.R
+++ R/funcs.R
@@ -45,30 +45,49 @@ CMA_parse_authors <- function(data, authors_csv ='data/lista_de_autores.csv'){
 CMA_subpop_cons <- function(data){
   require(tidyverse)
   require(kableExtra)
-  # data <- db.sp
+  # db <- read_csv('data/especies_nativas.csv')
+  # data <- db %>% filter(title == 'Blastocerus dichotomus')
+  
   stopifnot(nrow(data) == 1, ncol(data) > 1)
   
   if(is.na(data$group_sp_eval_subpob)){
     return(NULL)
   } else {
-  spListPop <- data$group_sp_eval_subpob %>% str_split('\n') %>% unlist() %>% str_trim()
+  spListPop <- data$group_sp_eval_subpob %>% 
+    str_split('\n') %>% 
+    unlist() %>% 
+    str_trim()
   
-  spListPop <- spListPop[ spListPop != ""]
+  spListPop <- spListPop[ !spListPop %in%  c("", ",")]
   
-  subPob <- spListPop[which(str_detect(spListPop, 'Subpoblación')) + 1]
-  catPob <- spListPop[which(str_detect(spListPop, 'Subpoblación')) + 3]
-  criPob <- spListPop[which(str_detect(spListPop, 'Subpoblación')) + 5]
+  nSubPob <- sum(str_detect(spListPop, 'Subpoblación'))
   
-  tbsPob <- vector()
-  for(i in seq_along(subPob)){
-    tbsPob[i] <- tribble(
-      ~'Subpoblación', ~'Categoría', ~'Criterios y subcriterios',
-      subPob[i], catPob[i], criPob[i]
-    ) %>% 
+  ies <- data.frame(
+    from = which(str_detect(spListPop, 'Subpoblación')),
+    to = c( (which(str_detect(spListPop, 'Subpoblación'))[-1]) -1,
+            length(spListPop))
+  )
+  
+  stopifnot(nrow(ies) == nSubPob)
+  
+  dfNames <- c('Subpoblación', 'Categoría', 'Criterios y subcriterios')
+  tbsPob <- vector('list', nSubPob)
+  for(i in 1:nSubPob){
+    # i=1
+    items <- c(ies$from[i]+c(1,3,5))
+    
+    tbsPob[[i]][[1]] <-  data.frame( t(spListPop[ items ]))
+    colnames(tbsPob[[i]][[1]]) <- dfNames
+    
+    tbsPob[[i]][[1]] <-  tbsPob[[i]][[1]] %>% 
       kbl(booktabs = T, format = 'latex') %>% 
       row_spec(0,bold=TRUE, extra_latex_after = "\\arrayrulecolor{white}") %>% 
       kable_styling(latex_options = c('striped', "HOLD_position"),
                     position = "center", full_width = T)
+    tbsPob[[i]][[2]] <- paste("**Justificación**\n\n",
+                              paste(
+                                spListPop[ (max(items)+2):ies$to[i] ],
+                                collapse = '\n\n'))
   }
   return(tbsPob)
   }
