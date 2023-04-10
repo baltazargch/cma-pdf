998eb0b5a0740918d599d64599df303a58cc6fd5
unknown <baltazargch@gmail.com>
2023-04-09 21:35:35 -0300

    Exploring italization of binomial names of all columns

---
+++ R/scientific_name_binarization.R
@@ -0,0 +1,31 @@
+CMA_italize_binomial <- function(data, exclude.col='title'){
+  require(tidyverse)
+  require(taxadb)
+  require(pbl)
+  data <- read_csv('data/especies_nativas.csv')
+  spList <- c(filter_rank(name='Chordata', 
+                          rank='Phylum', 
+                          'col')$scientificName, 
+              filter_rank(name='Chordata', 
+                          rank='Phylum', 
+                          'itis')$scientificName, 
+              data$title) %>% unique()
+  data.mod <- data %>% 
+    # filter(title == 'Blastocerus dichotomus') %>%
+    select(-sp_imagenes_de_la_especie , -title) %>% as.list()
+  
+  data.mod <- lapply(data.mod, \(x){
+    x <- data.mod$sp_id
+    
+    for(y in spList){ 
+    genus <- str_split(y, ' ', simplify = T)[1]
+      epite <- paste0(str_remove_all(genus, '[a-z]'), '. ',
+                      str_split(y, ' ', simplify = T)[2])
+      x <- x  %>% 
+        str_replace_all(genus, paste0('\\\\textit{', genus, '}')) %>%
+        str_replace_all(epite, paste0('\\\\textit{', epite, '}'))
+    }
+  })
+return(data.mod)
+}
+
